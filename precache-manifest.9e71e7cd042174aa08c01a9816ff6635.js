8a70532facead62563459b8247982e7e95ad3508
Matt Ilagan <matt.r.ilagan@gmail.com>
2020-07-24 16:05:34 -0700

    Updates

---
+++ precache-manifest.9e71e7cd042174aa08c01a9816ff6635.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7292a28478318a55e3c9e79dfd3a61",
+    "url": "/halo/index.html"
+  },
+  {
+    "revision": "a6dcb6149e23ea28bdff",
+    "url": "/halo/static/css/4.8921c4c4.chunk.css"
+  },
+  {
+    "revision": "d785801683a34211e545",
+    "url": "/halo/static/css/main.e1d5c106.chunk.css"
+  },
+  {
+    "revision": "404ba4cd1aaec5c00437",
+    "url": "/halo/static/js/0.f3358efd.chunk.js"
+  },
+  {
+    "revision": "8d86e0b9dfb08c31830b",
+    "url": "/halo/static/js/10.aa67f167.chunk.js"
+  },
+  {
+    "revision": "c3cb5c3893f4f55f629a",
+    "url": "/halo/static/js/11.3895f579.chunk.js"
+  },
+  {
+    "revision": "baeb02574b1067461178",
+    "url": "/halo/static/js/12.0cb283c3.chunk.js"
+  },
+  {
+    "revision": "c12eefa0a7f90a3a1224",
+    "url": "/halo/static/js/13.2067a7a4.chunk.js"
+  },
+  {
+    "revision": "66927cc8679793e9d833",
+    "url": "/halo/static/js/14.9823dadb.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/halo/static/js/14.9823dadb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10cb07eabd27e5977b15",
+    "url": "/halo/static/js/15.87d14f6d.chunk.js"
+  },
+  {
+    "revision": "17b3b0d904b0721d5ab3",
+    "url": "/halo/static/js/16.f52dae05.chunk.js"
+  },
+  {
+    "revision": "f2cffada63835529ed94",
+    "url": "/halo/static/js/17.dc0bd32e.chunk.js"
+  },
+  {
+    "revision": "6777bc6b9a7af3722ea8",
+    "url": "/halo/static/js/18.ac1a22ae.chunk.js"
+  },
+  {
+    "revision": "67f7e98f9522f16ba8d4",
+    "url": "/halo/static/js/19.11305fe8.chunk.js"
+  },
+  {
+    "revision": "431a6af30113edc1b8dd",
+    "url": "/halo/static/js/20.922cea20.chunk.js"
+  },
+  {
+    "revision": "f443c1c059c390ac9fe0",
+    "url": "/halo/static/js/21.c8041df1.chunk.js"
+  },
+  {
+    "revision": "687590d505e596b1148e",
+    "url": "/halo/static/js/22.65543a6f.chunk.js"
+  },
+  {
+    "revision": "8241151af329bf0db86b",
+    "url": "/halo/static/js/23.6ef84063.chunk.js"
+  },
+  {
+    "revision": "d43a4666c05ba68e81db",
+    "url": "/halo/static/js/24.562bb9f7.chunk.js"
+  },
+  {
+    "revision": "146aadde11095014711e",
+    "url": "/halo/static/js/25.3de5eb07.chunk.js"
+  },
+  {
+    "revision": "273347188125e88f88b8",
+    "url": "/halo/static/js/26.9680f8ba.chunk.js"
+  },
+  {
+    "revision": "87f6b8b077bb401b52c1",
+    "url": "/halo/static/js/27.211980d8.chunk.js"
+  },
+  {
+    "revision": "1a50e5e7766b26c3e99e",
+    "url": "/halo/static/js/28.cbad6416.chunk.js"
+  },
+  {
+    "revision": "a19c5694bc4012f02469",
+    "url": "/halo/static/js/29.1f03890f.chunk.js"
+  },
+  {
+    "revision": "ccafc87236d46225f3c9",
+    "url": "/halo/static/js/30.8f389448.chunk.js"
+  },
+  {
+    "revision": "b5c2fbe7af8e37403dd5",
+    "url": "/halo/static/js/31.eb6cfce5.chunk.js"
+  },
+  {
+    "revision": "c0efb98c27bdcfa7c58a",
+    "url": "/halo/static/js/32.280fb3f8.chunk.js"
+  },
+  {
+    "revision": "122f344f44d55fe7ca90",
+    "url": "/halo/static/js/33.411ed5ce.chunk.js"
+  },
+  {
+    "revision": "dbac56c0e389337555d6",
+    "url": "/halo/static/js/34.3e7b8130.chunk.js"
+  },
+  {
+    "revision": "fd4cb013ec152d78bef5",
+    "url": "/halo/static/js/35.5f7da0dc.chunk.js"
+  },
+  {
+    "revision": "aa5dd70c91acc76ca112",
+    "url": "/halo/static/js/36.0c266c63.chunk.js"
+  },
+  {
+    "revision": "1d136cd7d4fccc4ad5ab",
+    "url": "/halo/static/js/37.fa773ad1.chunk.js"
+  },
+  {
+    "revision": "1b41f7dfb1a576b2c6ae",
+    "url": "/halo/static/js/38.a2ae876a.chunk.js"
+  },
+  {
+    "revision": "2420aa686c7353eda7bc",
+    "url": "/halo/static/js/39.60659b6c.chunk.js"
+  },
+  {
+    "revision": "a6dcb6149e23ea28bdff",
+    "url": "/halo/static/js/4.7457db30.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/halo/static/js/4.7457db30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d27d52460f2e0c60c70f",
+    "url": "/halo/static/js/40.5f2cf6f6.chunk.js"
+  },
+  {
+    "revision": "1bd459e2b58bd2aa3161",
+    "url": "/halo/static/js/41.3be97cd9.chunk.js"
+  },
+  {
+    "revision": "698af7c22b09238dae4b",
+    "url": "/halo/static/js/42.e69a77d4.chunk.js"
+  },
+  {
+    "revision": "17ba3a6a09d81c43fae7",
+    "url": "/halo/static/js/43.81889f52.chunk.js"
+  },
+  {
+    "revision": "f253ef5142184ceaf8d0",
+    "url": "/halo/static/js/44.6dddbefa.chunk.js"
+  },
+  {
+    "revision": "60b2488fefd89508f3d7",
+    "url": "/halo/static/js/45.1a12c62a.chunk.js"
+  },
+  {
+    "revision": "32e51677801ba4a16588",
+    "url": "/halo/static/js/46.346970ce.chunk.js"
+  },
+  {
+    "revision": "50a97f5433bdfae4b4e0",
+    "url": "/halo/static/js/47.149b6fb2.chunk.js"
+  },
+  {
+    "revision": "8ae9bccda6dcc681f23c",
+    "url": "/halo/static/js/48.278d4ce9.chunk.js"
+  },
+  {
+    "revision": "f1caaa9909d4b58c6b1a",
+    "url": "/halo/static/js/49.4c63c0ce.chunk.js"
+  },
+  {
+    "revision": "62ce5de8db2ecaa89afe",
+    "url": "/halo/static/js/5.8f1395d8.chunk.js"
+  },
+  {
+    "revision": "54d4fdd85b976ca9836a",
+    "url": "/halo/static/js/50.b70acdb6.chunk.js"
+  },
+  {
+    "revision": "8c33705fedafe3b43bc5",
+    "url": "/halo/static/js/51.8d449be4.chunk.js"
+  },
+  {
+    "revision": "e401719a24c84391b3e0",
+    "url": "/halo/static/js/52.21ca257e.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/halo/static/js/52.21ca257e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55da1f88c9df046d5131",
+    "url": "/halo/static/js/53.5a54cfc1.chunk.js"
+  },
+  {
+    "revision": "29b4469b79bf7837cb9c",
+    "url": "/halo/static/js/54.c2ed2f76.chunk.js"
+  },
+  {
+    "revision": "bcbc6d556745306fed3d",
+    "url": "/halo/static/js/55.664b230c.chunk.js"
+  },
+  {
+    "revision": "8cfc6ee22360f999ad38",
+    "url": "/halo/static/js/56.c431ccb8.chunk.js"
+  },
+  {
+    "revision": "8649e8c7a474df4cc106",
+    "url": "/halo/static/js/57.a088a356.chunk.js"
+  },
+  {
+    "revision": "d8d0a22d12fff96a92e9",
+    "url": "/halo/static/js/58.ccea0479.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/halo/static/js/58.ccea0479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1000e7ebfcd17d34c2e7",
+    "url": "/halo/static/js/6.0aa9d422.chunk.js"
+  },
+  {
+    "revision": "61a538460b70b43f5431",
+    "url": "/halo/static/js/7.6c28e02c.chunk.js"
+  },
+  {
+    "revision": "272384ad5fbe131ed932",
+    "url": "/halo/static/js/8.6c6e5915.chunk.js"
+  },
+  {
+    "revision": "32babf4cb16914b77c69",
+    "url": "/halo/static/js/9.95c1a470.chunk.js"
+  },
+  {
+    "revision": "d785801683a34211e545",
+    "url": "/halo/static/js/main.e1c482f1.chunk.js"
+  },
+  {
+    "revision": "ff7b754705942f925318",
+    "url": "/halo/static/js/polyfills-css-shim.2f24cdb7.chunk.js"
+  },
+  {
+    "revision": "38be96720c493a5ed8e0",
+    "url": "/halo/static/js/runtime-main.39609e16.js"
+  }
+]);
---
+++ static/js/53.5a54cfc1.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonphalo=this.webpackJsonphalo||[]).push([[53],{165:function(n,t,o){"use strict";o.r(t),o.d(t,"startStatusTap",(function(){return i}));var e=o(5),i=function(){var n=window;n.addEventListener("statusTap",(function(){Object(e.j)((function(){var t=n.innerWidth,o=n.innerHeight,i=document.elementFromPoint(t/2,o/2);if(i){var c=i.closest("ion-content");c&&c.componentOnReady().then((function(){Object(e.g)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.5a54cfc1.chunk.js.map
